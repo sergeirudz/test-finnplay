7e51e05dc2f13e7a77a0761f10ae1194e16c840f
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-05-18 18:33:57 +0300

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './views/Home';
+import Login from './views/Login';
+import './styles/index.scss';
 
 function App() {
-
-
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
