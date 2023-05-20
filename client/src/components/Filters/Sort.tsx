c577a9c089ecfecf62f05d512930365994305b49
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-05-20 18:45:07 +0300

    WIP add Sorting component

---
+++ client/src/components/Filters/Sort.tsx
@@ -0,0 +1,81 @@
+import { ChangeEvent, useState } from 'react';
+import styles from './Sort.module.scss';
+
+type Props = {
+  sortGames: (sort: string) => void;
+  title: string;
+};
+
+export enum SortOptions {
+  NAME_ASC = 'name-asc',
+  NAME_DESC = 'name-desc',
+  NEWEST = 'newest',
+}
+
+const Sort = ({ sortGames, title }: Props) => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleSortChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSelectedOption(value);
+    sortGames(value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h4>{title}</h4>
+      <ul className={styles.sorting}>
+        <li
+          className={`${styles.option} ${
+            selectedOption === SortOptions.NAME_ASC && styles.active
+          }`}
+        >
+          <label>
+            <input
+              type="radio"
+              name="sort-option"
+              value={SortOptions.NAME_ASC}
+              checked={selectedOption === SortOptions.NAME_ASC}
+              onChange={handleSortChange}
+            />
+            A-Z
+          </label>
+        </li>
+        <li
+          className={`${styles.option} ${
+            selectedOption === SortOptions.NAME_DESC && styles.active
+          }`}
+        >
+          <label>
+            <input
+              type="radio"
+              name="sort-option"
+              value={SortOptions.NAME_DESC}
+              checked={selectedOption === SortOptions.NAME_DESC}
+              onChange={handleSortChange}
+            />
+            Z-A
+          </label>
+        </li>
+        <li
+          className={`${styles.option} ${
+            selectedOption === SortOptions.NEWEST && styles.active
+          }`}
+        >
+          <label>
+            <input
+              type="radio"
+              name="sort-option"
+              value="newest"
+              checked={selectedOption === 'newest'}
+              onChange={handleSortChange}
+            />
+            Newest
+          </label>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sort;
