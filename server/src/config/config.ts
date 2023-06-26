84bc3cd641d6bafa9470395e5e042c12984791d1
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-06-26 10:37:42 +0300

    add server config

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,3 +1,8 @@
 export const FRONTEND_URL = 'http://localhost:5173';
 export const SERVER_PORT = 4000;
 export const JWT_SECRET = 'fnqdG89lavVw702t'; //! openssl rand -base64 12
+export const PORT = 4000;
+export const ACCESS_TOKEN_EXPIRES_IN = 15;
+export const REFRESH_TOKEN_EXPIRES_IN = 59;
+export const ORIGIN = 'http://localhost:4000';
+export const NODE_ENV = 'development';
