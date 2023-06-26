0ec23ed9ec1de79b45c5dbe41be0bc2bae33d90f
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-06-26 22:32:09 +0300

    fix css to use variables

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
