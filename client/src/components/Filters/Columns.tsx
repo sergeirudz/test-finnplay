4fc6f60ea13306350f376ab75419867092fd06ac
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-06-26 16:11:47 +0300

    add Columns filter slider styles

--- client/src/components/Filters/Columns.tsx
+++ client/src/components/Filters/Columns.tsx
@@ -1,7 +1,10 @@
-// import { useDispatch } from 'react-redux';
 import styles from './Columns.module.scss';
-import { setFilterColumns } from '../../store/slices/filterSlice';
-import { useAppDispatch as useDispatch } from '../../store';
+import {
+  selectFilterColumns,
+  setFilterColumns,
+} from '../../store/slices/filterSlice';
+import { useAppSelector, useAppDispatch as useDispatch } from '../../store';
+import classNames from 'classnames';
 
 type Props = {
   title: string;
@@ -9,17 +12,32 @@ type Props = {
 
 const Columns = ({ title }: Props) => {
   const dispatch = useDispatch();
+  const columnNumber = useAppSelector(selectFilterColumns);
+
+  const columns = classNames({
+    [styles.columns_2]: columnNumber === '2',
+    [styles.columns_3]: columnNumber === '3',
+    [styles.columns_4]: columnNumber === '4',
+  });
 
   return (
     <div className={styles.container}>
       <h4>{title}</h4>
-      <input
-        type="range"
-        min="2"
-        max="4"
-        step="1"
-        onChange={(e) => dispatch(setFilterColumns(e.target.value))}
-      />
+      <div className={`${styles.range} ${columns}`}>
+        <input
+          type="range"
+          min="2"
+          max="4"
+          step="1"
+          onChange={(e) => dispatch(setFilterColumns(e.target.value))}
+        />
+        <div className={`${styles.stepNumbers}`}>
+          <span>2</span>
+          <span>3</span>
+          <span>4</span>
+        </div>
+        <div className={`${styles.range}`}></div>
+      </div>
     </div>
   );
 };
